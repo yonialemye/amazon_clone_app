87ec2c8db44097387ec822a5765010d503560258
yonialemye <yonialemye@gmail.com>
2022-06-11 23:20:39 +0300

    project setup

---
+++ lib/constants/global_variables.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class GlobalVariables {
+  // colors
+  static const kBackgroundColor = Colors.white;
+  static const kSecondaryColor = Color(0xFFFA7911);
+}
