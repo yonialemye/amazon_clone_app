19ac8aef48a770dce51144d0c520bbbd4171bb48
yonialemye <yonialemye@gmail.com>
2022-06-12 00:01:04 +0300

    build auth screen ui

--- lib/constants/global_variables.dart
+++ lib/constants/global_variables.dart
@@ -4,4 +4,5 @@ class GlobalVariables {
   // colors
   static const kBackgroundColor = Colors.white;
   static const kSecondaryColor = Color(0xFFFA7911);
+  static const kGreyBackgroundColor = Color(0xffebecee);
 }
--- lib/features/auth/screens/auth_screen.dart
+++ lib/features/auth/screens/auth_screen.dart
@@ -1,4 +1,12 @@
+import 'package:amazon_clone_app/commons/widgets/custom_button.dart';
+import 'package:amazon_clone_app/commons/widgets/custom_textfield.dart';
+import 'package:amazon_clone_app/constants/global_variables.dart';
 import 'package:flutter/material.dart';
+
+enum Auth {
+  signin,
+  signup,
+}
 
 class AuthScreen extends StatefulWidget {
   static const String routeName = '/auth-screen';
@@ -9,8 +17,135 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  Auth _auth = Auth.signup;
+  final GlobalKey<FormState> _singUpFormKey = GlobalKey<FormState>();
+  final GlobalKey<FormState> _singInFormKey = GlobalKey<FormState>();
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    _nameController.dispose();
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: GlobalVariables.kGreyBackgroundColor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                'Getting Stareted',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              ListTile(
+                tileColor: _auth == Auth.signup
+                    ? GlobalVariables.kBackgroundColor
+                    : GlobalVariables.kGreyBackgroundColor,
+                title: const Text(
+                  'Create account',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                leading: Radio(
+                  value: Auth.signup,
+                  groupValue: _auth,
+                  activeColor: GlobalVariables.kSecondaryColor,
+                  onChanged: (Auth? value) {
+                    setState(() => _auth = value!);
+                  },
+                ),
+              ),
+              if (_auth == Auth.signup)
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  color: GlobalVariables.kBackgroundColor,
+                  child: Form(
+                    key: _singUpFormKey,
+                    child: Column(
+                      children: [
+                        CustomTextField(
+                          controller: _nameController,
+                          hintText: 'Name',
+                        ),
+                        const SizedBox(height: 10),
+                        CustomTextField(
+                          controller: _emailController,
+                          hintText: 'Email',
+                        ),
+                        const SizedBox(height: 10),
+                        CustomTextField(
+                          controller: _passwordController,
+                          hintText: 'Password',
+                        ),
+                        const SizedBox(height: 20),
+                        CustomButton(
+                          text: 'Register',
+                          onPressed: () {},
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ListTile(
+                tileColor: _auth == Auth.signin
+                    ? GlobalVariables.kBackgroundColor
+                    : GlobalVariables.kGreyBackgroundColor,
+                title: const Text(
+                  'Signin',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                leading: Radio(
+                  value: Auth.signin,
+                  groupValue: _auth,
+                  activeColor: GlobalVariables.kSecondaryColor,
+                  onChanged: (Auth? value) {
+                    setState(() => _auth = value!);
+                  },
+                ),
+              ),
+              if (_auth == Auth.signin)
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  color: GlobalVariables.kBackgroundColor,
+                  child: Form(
+                    key: _singUpFormKey,
+                    child: Column(
+                      children: [
+                        CustomTextField(
+                          controller: _emailController,
+                          hintText: 'Email',
+                        ),
+                        const SizedBox(height: 10),
+                        CustomTextField(
+                          controller: _passwordController,
+                          hintText: 'Password',
+                        ),
+                        const SizedBox(height: 20),
+                        CustomButton(
+                          text: 'Register',
+                          onPressed: () {},
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
