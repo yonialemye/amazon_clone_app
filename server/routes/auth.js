76ed9a00d8812d2c840762f45903e80550c3e7e8
yonialemye <yonialemye@gmail.com>
2022-06-14 22:18:02 +0300

    creating signin route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const User = require('../models/user');
 const bcryptjs = require("bcryptjs");
+const jwt = require('jsonwebtoken');
 
 const authRouter = express.Router();
 
+// Signup route
 authRouter.post("/api/signup", async (req, res) => {
     try {
         const { name, email, password } = req.body;
@@ -20,6 +22,32 @@ authRouter.post("/api/signup", async (req, res) => {
         user = await user.save();
 
         res.json(user);
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+
+// login route
+authRouter.post('/api/signin', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({ msg: "User does not exist" });
+        }
+
+        const isMatch = await bcryptjs.compare(password, user.password);
+
+        if (!isMatch) {
+            return res.status(400).json({ msg: "Incorrecr password" });
+        }
+
+        const token = jwt.sign({ id: user._id }, "passwordKey");
+
+        res.json({ token, ...user._doc });
+
     } catch (e) {
         res.status(500).json({ error: e.message });
     }
