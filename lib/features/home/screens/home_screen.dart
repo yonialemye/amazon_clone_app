0abca48b0fbb37db2bf39dd3d2b1b7c16b02c45d
yonialemye <yonialemye@gmail.com>
2022-06-17 12:30:07 +0300

    build home screen appbar

--- lib/features/home/screens/home_screen.dart
+++ lib/features/home/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:amazon_clone_app/constants/global_variables.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
@@ -11,8 +12,65 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final user = Provider.of<UserProvider>(context).user;
     return Scaffold(
-      body: Center(
-        child: Text(user.toJson()),
+      appBar: AppBar(
+        backgroundColor: const Color(0xFF232f3f),
+        toolbarHeight: kToolbarHeight,
+        centerTitle: true,
+        title: Column(
+          children: [
+            const SizedBox(height: 15),
+            Image.asset(
+              'assets/images/amazon_in.png',
+              height: 45,
+              color: Colors.white,
+            ),
+          ],
+        ),
+        bottom: PreferredSize(
+          preferredSize: const Size.fromHeight(70.0),
+          child: Container(
+            margin: const EdgeInsets.only(left: 20, right: 20, bottom: 10, top: 20),
+            child: TextFormField(
+              decoration: InputDecoration(
+                contentPadding: const EdgeInsets.only(top: 15),
+                border: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
+                focusedBorder: const OutlineInputBorder(
+                  borderSide: BorderSide(color: GlobalVariables.kSecondaryColor),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
+                fillColor: Colors.white,
+                filled: true,
+                hintText: 'What are you looking for?',
+                isDense: true,
+                prefixIcon: const Icon(Icons.search),
+                suffixIcon: Container(
+                  margin: const EdgeInsets.all(10),
+                  decoration: BoxDecoration(
+                    color: GlobalVariables.kSecondaryColor,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: IconButton(
+                    onPressed: () {},
+                    constraints: const BoxConstraints(),
+                    icon: const Icon(
+                      Icons.keyboard_voice_outlined,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: [],
       ),
     );
   }
